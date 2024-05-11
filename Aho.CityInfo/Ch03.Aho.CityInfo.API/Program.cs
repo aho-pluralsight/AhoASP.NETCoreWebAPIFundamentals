881724b1da09294886218e148dbe12dec89276f7
aho <aho@aho.dz>
2024-05-11 21:21:54 +0200

    [CH03][VID] -> Demo: Manipulating ProblemDetails Responses

--- Aho.CityInfo/Ch03.Aho.CityInfo.API/Program.cs
+++ Aho.CityInfo/Ch03.Aho.CityInfo.API/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = ctxt =>
+    {
+        ctxt.ProblemDetails.Extensions.Add("customInfo", "Take it or leave it!");
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
