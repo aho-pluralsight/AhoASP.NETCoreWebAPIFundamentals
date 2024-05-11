862e7ed8794e7283221cdc5d5bac64f803ae9663
aho <aho@aho.dz>
2024-05-11 08:52:52 +0200

    [CH02][AHO] -> Messing more with the API's Request/Response

--- Aho.CityInfo/Ch02.Aho.CityInfo.API/Program.cs
+++ Aho.CityInfo/Ch02.Aho.CityInfo.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,28 +28,75 @@ if (app.Environment.IsDevelopment())
 
 app.Use(async (context, next) =>
 {
-    context.Response.WriteAsync("01. Outbound\n");
-    await next(context);
-    context.Response.WriteAsync("01. Return\n");
+    if (context.Request.Path.Value == "/aho.js")
+    {
+        string execDir = AppDomain.CurrentDomain.BaseDirectory;
+        await context.Response.WriteAsync(System.IO.File.ReadAllText($"{execDir}\\scripts\\aho.js"));
+    }
+    else if (context.Request.Path.Value == "/aho.css")
+    {
+        string execDir = AppDomain.CurrentDomain.BaseDirectory;
+        await context.Response.WriteAsync(System.IO.File.ReadAllText($"{execDir}\\scripts\\aho.css"));
+    }
+    else
+    {
+        await next(context);
+    }
 });
 
 app.Use(async (context, next) =>
 {
-    context.Response.WriteAsync("02. Outbound\n");
-    await next(context);
-    context.Response.WriteAsync("02. Return\n");
+    if (context.Request.Path.Value == "/pretty")
+    {
+        await context.Response.WriteAsync(
+            "<!DOCTYPE html>" +
+            "<html lang=\"en\" class=\"notranslate\" translate=\"no\"");
+        await context.Response.WriteAsync(
+            "<head>" +
+            "<link rel='stylesheet' href='aho.css'>" +
+            "<meta name=\"google\" content=\"notranslate\" />" +
+            "</head>");
+        await context.Response.WriteAsync(
+            "<body>");
+        await context.Response.WriteAsync("<div id='pipeContent'>");
+        await next(context);
+        await context.Response.WriteAsync("</div>");
+        await context.Response.WriteAsync("<script type='text/javascript' src='aho.js'></script>");
+        await context.Response.WriteAsync("<script>jolifying();</script>");
+        await context.Response.WriteAsync(
+            "</body>" +
+            "</html>");
+    }
+    else
+    {
+        await next(context);
+    }
 });
 
 app.Use(async (context, next) =>
 {
-    context.Response.WriteAsync("03. Outbound\n");
+    await context.Response.WriteAsync("P03. Outbound\n");
     await next(context);
-    context.Response.WriteAsync("03. Return\n");
+    await context.Response.WriteAsync("P03. Return\n");
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("P02. Outbound\n");
+    await next(context);
+    await context.Response.WriteAsync("P02. Return\n");
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("P01. Outbound\n");
+    await next(context);
+    await context.Response.WriteAsync("P01. Return\n");
 });
 
 app.Run(async context =>
 {
-    context.Response.WriteAsync("-> X. Bullseye!\n");
+    await context.Response.WriteAsync("P00. Bullseye\n");
 });
 
 app.Run();
---
+++ Aho.CityInfo/Ch02.Aho.CityInfo.API/scripts/aho.js
@@ -0,0 +1,37 @@
+﻿function jolifying() {
+    var element = document.getElementById("pipeContent");
+
+    console.info(element.textContent);
+
+    if (element) {
+        let parts = element.textContent.split('P').filter(x => x != '');
+        let htmlContent = '<div id="paveDansLaMare"><h2 class="title">ASPNET Core \'API\' request/response pipeline in action!</h2></div>';
+        htmlContent += '<div>';
+        let midIn = parts.findIndex(pa => pa.includes('00'));
+        let displayTag = 'p';
+
+        parts.forEach((part, i) => {
+            let boxId = String(i.toString()).padStart(2, '0');
+            if (i < midIn) {
+                htmlContent += `<div class="element box-${boxId}"><${displayTag} class="displayTag outbound">P${part}</${displayTag}>`;
+            }
+            if (i == midIn) {
+                htmlContent += `<div class="element box-${boxId}"><${displayTag} class="displayTag bullseye"><mark>P${part}</mark></${displayTag}></div>`;
+            }
+            if (i > midIn) {
+                htmlContent += `<${displayTag} class="displayTag return">P${part}</${displayTag}></div>`;
+            }
+        });
+        htmlContent += '</div>';
+        element.innerHTML = htmlContent;
+    } else {
+        alert("This shit is not working yo!");
+        console.error("Element with ID 'pipeContent' not found.");
+    }
+}
+
+window.addEventListener("load", function () {
+    console.info(
+        "AHO is having fun!",
+    );
+});
