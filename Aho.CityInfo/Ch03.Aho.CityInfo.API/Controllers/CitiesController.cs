150d2b85bd01822673bec7dabb88475679c8362e
aho <aho@aho.dz>
2024-05-11 12:58:52 +0200

    [CH03][VID] -> Demo: Returning Resources (Part 2)

---
+++ Aho.CityInfo/Ch03.Aho.CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Ch03.Aho.CityInfo.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new { Id = 1, Name = "New York City" },
+                new { Id = 2, Name = "Lyon" }
+            });
+        }
+    }
+}
