dcb2536753970bb635acd6b544cfa580ce068450
aho <aho@aho.dz>
2024-05-12 11:17:13 +0200

    [CH03][VID] -> Demo: Formatters and Content Negotiation

--- Aho.CityInfo/Ch03.Aho.CityInfo.API/Program.cs
+++ Aho.CityInfo/Ch03.Aho.CityInfo.API/Program.cs
@@ -1,16 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    //[AHO] Not acceptable (Status code 406) when the requested format (Accept header) is not available
+    options.ReturnHttpNotAcceptable = true;
+    //[AHO] Explicit Output Formatterd
+    options.OutputFormatters.Clear();
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()); // The first registered Formatter => Default Formatter
+    options.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new JsonSerializerOptions
+    {
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    }));
+});
 
 builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = ctxt =>
-    {
-        ctxt.ProblemDetails.Extensions.Add("customInfo", "Take it or leave it!");
-    };
-});
+ {
+     options.CustomizeProblemDetails = ctxt =>
+     {
+         ctxt.ProblemDetails.Extensions.Add("customInfo", "Take it or leave it!");
+     };
+ });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
