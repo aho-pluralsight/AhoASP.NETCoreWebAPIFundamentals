f2557d1642a7ea4a09131a5f8c3d6f79b7402d77
aho <aho@aho.dz>
2024-05-19 16:40:04 +0200

    [CH04][VID] -> Demo: Validating Input

--- Aho.CityInfo/Ch03.Aho.CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ Aho.CityInfo/Ch03.Aho.CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Ch03.Aho.CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ch03.Aho.CityInfo.API.Models
 {
     public class PointOfInterestForCreationDto
     {
+        [Required(ErrorMessage = "The property 'Name' is required!")]
+        [MaxLength(50, ErrorMessage = "The 'Name' field must not exceed 50 characters!")]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(200, ErrorMessage = "The 'Name' field must not exceed 200 characters!")]
         public string? Description { get; set; }
     }
 }
