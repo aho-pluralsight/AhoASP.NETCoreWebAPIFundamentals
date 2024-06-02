3e46787160d5d066b9c74a7359804d0c69970c2e
aho <aho@aho.dz>
2024-06-02 09:16:04 +0200

    [CH06][VID] -> Demo: Creating Entity Classes

---
+++ Aho.CityInfo/Ch06.Aho.CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ch06.Aho.CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
