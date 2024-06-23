03e6ac2d4f119a507bde6a651e2aabb855fab2cb
aho <aho@aho.dz>
2024-06-23 11:21:11 +0200

    [CH07][VID] -> Demo: Returning Data from the Repository When Requesting Resources (Part 1)

--- Aho.CityInfo/Ch06.Aho.CityInfo.API/Controllers/CitiesController.cs
+++ Aho.CityInfo/Ch06.Aho.CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using Ch06.Aho.CityInfo.API.Models;
+using Ch06.Aho.CityInfo.API.Services.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ch06.Aho.CityInfo.API.Controllers
@@ -9,44 +10,56 @@ namespace Ch06.Aho.CityInfo.API.Controllers
     public class CitiesController : ControllerBase
     {
         private readonly ILogger<CitiesController> _logger;
-        private readonly CitiesDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(ILogger<CitiesController> logger, CitiesDataStore citiesDataStore)
+        public CitiesController(ILogger<CitiesController> logger, ICityInfoRepository cityInfoRepository)
         {
-            this._logger = logger ?? throw new ArgumentNullException();
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException() ;
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+            var results = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var city in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto()
+                {
+                    Description = city.Description,
+                    Name = city.Name,
+                    Id = city.Id
+                });
+            }
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id)
         {
-            try
-            {
-                if (id == 0)
-                {
-                    throw new ArgumentException("This is nasty yo!");
-                }
+            //try
+            //{
+            //    if (id == 0)
+            //    {
+            //        throw new ArgumentException("This is nasty yo!");
+            //    }
 
-                var city = _citiesDataStore.Cities.FirstOrDefault(city => city.Id == id);
+            //    var city = _citiesDataStore.Cities.FirstOrDefault(city => city.Id == id);
 
-                if (city == null)
-                {
-                    return NotFound();
-                }
+            //    if (city == null)
+            //    {
+            //        return NotFound();
+            //    }
 
-                return Ok(city);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical($"Bad exception occured while handling a call to get the city with the id {id}!", ex);
-                return StatusCode(500, $"Something nasty occurred on the server side!");
-            }
+            //    return Ok(city);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogCritical($"Bad exception occured while handling a call to get the city with the id {id}!", ex);
+            //    return StatusCode(500, $"Something nasty occurred on the server side!");
+            //}
+            return Ok();
         }
     }
 }
